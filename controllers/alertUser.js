3a8c2510cb8db487471b8b9f4f839dfa28fe087f
Mark <mso872@gmail.com>
2020-04-22 09:27:43 -0700

    added alert controller and burger model

---
+++ controllers/alertUser.js
@@ -0,0 +1,21 @@
+const express = require(`express`);
+const routy = express.Router();
+// const moment = require(`moment-timezone`)
+
+// Import the model (burger.js) to use its database functions.
+const alert = require(`../../models/alert.js`);
+
+routy.put(`/api/alerts/:id`, (req, res) => {
+    const condition = `id = ` + req.params.id;
+    alert.updateOne(req.body, condition, result => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end()
+        } else {
+            res.status(200).end()
+        }
+    })
+})
+
+// Export routes for server.js to use.
+module.exports = routy

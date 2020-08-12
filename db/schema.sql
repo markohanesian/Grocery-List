f8c430239172cedaa950b5521a83a251ed24ac5d
Mark <mso872@gmail.com>
2020-08-12 05:39:26 -0700

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,10 @@
 ### Schema
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE meals_db;
+USE meals_db;
 
-CREATE TABLE burgers (
+CREATE TABLE meals (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-    devoured BOOL DEFAULT false,
+    meal_name varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );

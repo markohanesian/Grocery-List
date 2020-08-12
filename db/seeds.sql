b650a05dea82f59f60d0f202d59f601277c6b5ad
Mark <mso872@gmail.com>
2020-08-12 05:38:32 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO meals (meal_name) VALUES ('Acai Bowl');
+INSERT INTO meals (meal_name) VALUES ('Avocado Salad');
+INSERT INTO meals (meal_name) VALUES ('Apple');

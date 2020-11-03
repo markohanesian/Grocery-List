0eb3e1b6bb3e7c0f2d1a4b2aa5e8ca16ecb73f99
markohanesian <mso872@gmail.com>
2020-11-03 11:57:17 -0800

    replaced client information with import from keys.js

--- config/middleware/passport-setup.js
+++ config/middleware/passport-setup.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+// keys file import
+const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
         // options for the Google strat
-        // from Google dev console for Grocery List project
-        clientID: '622929773837-s2qstapjimpu7sbrm3v5b6s95ra50h17.apps.googleusercontent.com',
-        clientSecret: 'OVerzm1yARB0kwbKHGJhVQqP',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
     }), () => {
         // passport callback function
     })

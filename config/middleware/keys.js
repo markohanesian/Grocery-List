ad71a7a12768498aaf458fa1d09abd90f4995dd5
markohanesian <mso872@gmail.com>
2020-11-03 11:57:37 -0800

    Created keys.js to securely store client information

--- config/middleware/keys.js
+++ config/middleware/keys.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     google: {
+        // from Google dev console for Grocery List project
         clientID: "622929773837-s2qstapjimpu7sbrm3v5b6s95ra50h17.apps.googleusercontent.com",
         clientSecret: "OVerzm1yARB0kwbKHGJhVQqP"
     }

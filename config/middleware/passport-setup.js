4fb508f144bd11b1da7fa8a7c1a1c326827766c8
markohanesian <mso872@gmail.com>
2020-11-03 11:51:30 -0800

    Added credential information from Google Dev Console Grocery List project

--- config/middleware/passport-setup.js
+++ config/middleware/passport-setup.js
@@ -4,6 +4,9 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
         // options for the Google strat
+        // from Google dev console for Grocery List project
+        clientID: '622929773837-s2qstapjimpu7sbrm3v5b6s95ra50h17.apps.googleusercontent.com',
+        clientSecret: 'OVerzm1yARB0kwbKHGJhVQqP',
     }), () => {
         // passport callback function
     })

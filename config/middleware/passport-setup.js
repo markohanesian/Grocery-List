df4d56c781147072bce069cff7c51c06fd71593d
markohanesian <mso872@gmail.com>
2020-11-03 12:01:35 -0800

    added callbackURL object, removed redundant parentheses

--- config/middleware/passport-setup.js
+++ config/middleware/passport-setup.js
@@ -6,9 +6,10 @@ const keys = require('./keys');
 passport.use(
     new GoogleStrategy({
         // options for the Google strat
+        callbackURL: '/auth/google.redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
-    }), () => {
+    }, () => {
         // passport callback function
     })
 )

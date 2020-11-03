73a17a102d1e0d1a7267b8322edd4b37cf1d13a2
markohanesian <mso872@gmail.com>
2020-11-03 11:54:40 -0800

    Created keys file to securely store clientID and clientSecret and export as objects

---
+++ config/middleware/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: "622929773837-s2qstapjimpu7sbrm3v5b6s95ra50h17.apps.googleusercontent.com",
+        clientSecret: "OVerzm1yARB0kwbKHGJhVQqP"
+    }
+}

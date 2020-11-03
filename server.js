d8ee3d25d16ac94721313aac8bf613c4a4182d8f
markohanesian <mso872@gmail.com>
2020-11-03 12:13:19 -0800

    Created passportSetup

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
-var app = express();
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+const passportSetup = require('./config/middleware/passport-setup');
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");

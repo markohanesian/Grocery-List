6b31b39b3c727db75b917551ea66a314dae456b9
markohanesian <mso872@gmail.com>
2020-11-02 17:34:36 -0800

    Changed var to const

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./routes/routes.js");
+const routes = require('./routes/routes.js');
 
 // create home route
 app.get('/', (req, res) => {

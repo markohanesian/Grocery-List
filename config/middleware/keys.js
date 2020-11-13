783696e8a7d38b43ae0799e5f4444526e9f373cc
markohanesian <mso872@gmail.com>
2020-11-13 12:25:12 -0800

    Added mongodb URI to keys

--- config/middleware/keys.js
+++ config/middleware/keys.js
@@ -5,5 +5,8 @@ module.exports = {
         // from Google dev console for Grocery List project
         clientID: "622929773837-s2qstapjimpu7sbrm3v5b6s95ra50h17.apps.googleusercontent.com",
         clientSecret: "OVerzm1yARB0kwbKHGJhVQqP"
+    },
+    mongodb: {
+        dbURI: 'mongo "mongodb+srv://grocery-list.juek7.mongodb.net/<grocery-list>" --username new_user',
     }
-}
+};

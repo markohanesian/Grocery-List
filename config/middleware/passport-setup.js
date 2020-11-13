e7680a18091a9efda50b2c0ef18e02eb90a9c069
markohanesian <mso872@gmail.com>
2020-11-13 13:41:23 -0800

    Added user object; updated Google strategy

--- config/middleware/passport-setup.js
+++ config/middleware/passport-setup.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 // keys file import
 const keys = require('./keys');
+// importing user model
+const user = require('../../models/user.model');
 
 passport.use(
     new GoogleStrategy({
@@ -9,7 +11,9 @@ passport.use(
         callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
-    }, () => {
+    }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
+        console.log('passport callback function fired');
+        console.log(profile)
     })
 )
